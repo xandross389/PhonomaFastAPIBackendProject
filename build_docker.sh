66b73621a9fd05790e3cf2d98b1364c5b3f17efe
Alejandro Gomez Jimenez <xandross389@gmail.com>
2023-04-30 22:34:52 -0400

    added docker support

---
+++ build_docker.sh
@@ -0,0 +1,11 @@
+######
+# Filename: build_docker.sh
+# Created Date: Sunday, April 30th 2023, 10:18:17 pm
+# Author: Alejandro Gómez Jiménez (xandross389@gmail.com)
+# -----
+# Copyright (c) 2023 - MIT License
+# -----
+######
+
+#!/bin/bash
+sudo docker build -t phonoma-fastapi:latest .
